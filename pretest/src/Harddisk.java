d62298ca4cf0ef2c3837634cc9f76d25e50176b3
wsnubmntr <nuuu008@gmail.com>
2026-05-19 14:42:03 +0700

    MODUL9

---
+++ pretest/src/Harddisk.java
@@ -0,0 +1,10 @@
+class Harddisk {
+    String merk;
+    int kapasitas;
+
+    // Constructor
+    Harddisk(String merk, int kapasitas) {
+        this.merk = merk;
+        this.kapasitas = kapasitas;
+    }
+}
---
+++ pretest/src/Monitor.java
@@ -0,0 +1,10 @@
+class Monitor {
+    String merk;
+    int ukuran;
+
+    // Constructor
+    Monitor(String merk, int ukuran) {
+        this.merk = merk;
+        this.ukuran = ukuran;
+    }
+}
